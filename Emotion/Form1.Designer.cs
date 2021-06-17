78e23b4cafcfec182d90941eb7cdea9808faf584
Zhezlova Daria <zh.daria4@gmail.com>
2021-06-17 15:22:21 +0300

    emotion app

---
+++ Emotion/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Emotion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.timerOb = new System.Windows.Forms.Timer(this.components);
+            this.NeturalT = new System.Windows.Forms.Timer(this.components);
+            this.HappyT = new System.Windows.Forms.Timer(this.components);
+            this.SadT = new System.Windows.Forms.Timer(this.components);
+            this.WinkT = new System.Windows.Forms.Timer(this.components);
+            this.SleepT = new System.Windows.Forms.Timer(this.components);
+            this.NetPB = new System.Windows.Forms.PictureBox();
+            this.HapPB = new System.Windows.Forms.PictureBox();
+            this.SadPB = new System.Windows.Forms.PictureBox();
+            this.WinkPB = new System.Windows.Forms.PictureBox();
+            this.SleepPB = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Strelka = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NetPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HapPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SadPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WinkPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SleepPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Strelka)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // timerOb
+            // 
+            this.timerOb.Interval = 1000;
+            this.timerOb.Tick += new System.EventHandler(this.timerOb_Tick);
+            // 
+            // NeturalT
+            // 
+            this.NeturalT.Interval = 900;
+            this.NeturalT.Tick += new System.EventHandler(this.NeturalT_Tick);
+            // 
+            // HappyT
+            // 
+            this.HappyT.Interval = 900;
+            this.HappyT.Tick += new System.EventHandler(this.HappyT_Tick);
+            // 
+            // SadT
+            // 
+            this.SadT.Interval = 900;
+            this.SadT.Tick += new System.EventHandler(this.SadT_Tick);
+            // 
+            // WinkT
+            // 
+            this.WinkT.Interval = 900;
+            this.WinkT.Tick += new System.EventHandler(this.WinkT_Tick);
+            // 
+            // SleepT
+            // 
+            this.SleepT.Interval = 900;
+            this.SleepT.Tick += new System.EventHandler(this.SleepT_Tick);
+            // 
+            // NetPB
+            // 
+            this.NetPB.Image = ((System.Drawing.Image)(resources.GetObject("NetPB.Image")));
+            this.NetPB.Location = new System.Drawing.Point(659, 12);
+            this.NetPB.Name = "NetPB";
+            this.NetPB.Size = new System.Drawing.Size(65, 65);
+            this.NetPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.NetPB.TabIndex = 3;
+            this.NetPB.TabStop = false;
+            // 
+            // HapPB
+            // 
+            this.HapPB.Image = ((System.Drawing.Image)(resources.GetObject("HapPB.Image")));
+            this.HapPB.Location = new System.Drawing.Point(753, 12);
+            this.HapPB.Name = "HapPB";
+            this.HapPB.Size = new System.Drawing.Size(65, 65);
+            this.HapPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.HapPB.TabIndex = 4;
+            this.HapPB.TabStop = false;
+            // 
+            // SadPB
+            // 
+            this.SadPB.Image = ((System.Drawing.Image)(resources.GetObject("SadPB.Image")));
+            this.SadPB.Location = new System.Drawing.Point(659, 107);
+            this.SadPB.Name = "SadPB";
+            this.SadPB.Size = new System.Drawing.Size(65, 65);
+            this.SadPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SadPB.TabIndex = 5;
+            this.SadPB.TabStop = false;
+            // 
+            // WinkPB
+            // 
+            this.WinkPB.Image = ((System.Drawing.Image)(resources.GetObject("WinkPB.Image")));
+            this.WinkPB.Location = new System.Drawing.Point(753, 107);
+            this.WinkPB.Name = "WinkPB";
+            this.WinkPB.Size = new System.Drawing.Size(65, 65);
+            this.WinkPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.WinkPB.TabIndex = 6;
+            this.WinkPB.TabStop = false;
+            // 
+            // SleepPB
+            // 
+            this.SleepPB.Image = ((System.Drawing.Image)(resources.GetObject("SleepPB.Image")));
+            this.SleepPB.Location = new System.Drawing.Point(659, 201);
+            this.SleepPB.Name = "SleepPB";
+            this.SleepPB.Size = new System.Drawing.Size(65, 65);
+            this.SleepPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SleepPB.TabIndex = 7;
+            this.SleepPB.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(682, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(778, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(682, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(778, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(682, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(778, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(741, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Общее время:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(658, 285);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 268);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Strelka
+            // 
+            this.Strelka.BackColor = System.Drawing.Color.Transparent;
+            this.Strelka.Image = ((System.Drawing.Image)(resources.GetObject("Strelka.Image")));
+            this.Strelka.Location = new System.Drawing.Point(717, 394);
+            this.Strelka.Name = "Strelka";
+            this.Strelka.Size = new System.Drawing.Size(62, 50);
+            this.Strelka.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Strelka.TabIndex = 16;
+            this.Strelka.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Strelka);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SleepPB);
+            this.Controls.Add(this.WinkPB);
+            this.Controls.Add(this.SadPB);
+            this.Controls.Add(this.HapPB);
+            this.Controls.Add(this.NetPB);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NetPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HapPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SadPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WinkPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SleepPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Strelka)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Timer timerOb;
+        private System.Windows.Forms.Timer NeturalT;
+        private System.Windows.Forms.Timer HappyT;
+        private System.Windows.Forms.Timer SadT;
+        private System.Windows.Forms.Timer WinkT;
+        private System.Windows.Forms.Timer SleepT;
+        private System.Windows.Forms.PictureBox NetPB;
+        private System.Windows.Forms.PictureBox HapPB;
+        private System.Windows.Forms.PictureBox SadPB;
+        private System.Windows.Forms.PictureBox WinkPB;
+        private System.Windows.Forms.PictureBox SleepPB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Strelka;
+    }
+}
+
---
+++ Emotion/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace Emotion
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, Byte> CameraImage;
+        Capture camera;
+
+        CascadeClassifier face;
+        CascadeClassifier eye;
+        CascadeClassifier smile;
+        CascadeClassifier em;
+        CascadeClassifier mouth;
+        CascadeClassifier mouth2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            face = new CascadeClassifier("haarcascade_frontalface_default.xml");
+            eye = new CascadeClassifier("haarcascade_eye.xml");
+            smile = new CascadeClassifier("haarcascade_smile.xml");
+            em = new CascadeClassifier("haarcascade_frontalface_emotion.xml");
+            mouth = new CascadeClassifier("haarcascade_mouth.xml");
+            mouth2 = new CascadeClassifier("Mouth.xml");
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            Strelka.BackColor = Color.Transparent;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            Application.Idle += Application_Idle;
+            button1.Enabled = false;
+            timerOb.Start();
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            CameraImage = camera.QueryFrame().ToImage<Bgr, byte>().Resize(640, 480, Inter.Cubic);
+          
+            Rectangle[] facesDetected = face.DetectMultiScale(CameraImage, 1.2, 10, new Size(10, 10), new Size(640, 480));//И это то с вебкой 640х360, лол
+            // Rectangle[] emDetected = em.DetectMultiScale(CameraImage, 1.2, 10, new Size(10, 10), new Size(640, 480));
+            bool[] Timers = new bool[5];//булы на то, какие таймера запускать/останавливать с этого кадра
+            imageBox1.Controls.Clear();//убрать предыдущие эмодзи
+            double koof = 1.0 / facesDetected.Length;
+            double proc = 0;
+            double netproc = 0;
+            foreach (Rectangle f in facesDetected)
+            {
+                    PictureBox Emotion = new PictureBox();
+                    CameraImage.Draw(f, new Bgr(Color.Black), 2);//отрисовка квадрата вокруг лица
+
+                    Point Txt = new Point(f.X, f.Y);
+
+                    CameraImage.ROI = f;//ограничиваем область поиска найденным лицом
+                
+                    Rectangle[] eyesDetected = eye.DetectMultiScale(CameraImage, 1.2, 10, new Size(8, 8), new Size(160, 120));//находит все глаза и записывает координаты в массив
+                    Rectangle[] smileDetected = smile.DetectMultiScale(CameraImage, 1.33, 40, new Size(16, 12), new Size(320, 240));//находит улыбки и записывает координаты в массив
+                    Rectangle[] mouthDetected = mouth.DetectMultiScale(CameraImage, 1.33, 40, new Size(16, 12), new Size(320, 240));//наход рты раз
+                    Rectangle[] mouth2Detected = mouth2.DetectMultiScale(CameraImage, 1.33, 40, new Size(16, 12), new Size(320, 240));//находит рты два
+
+                    CameraImage.ROI = Rectangle.Empty;//возвращаем область в нормальное состояние для работы
+                    bool TimerNet = true;//для определения, остается ли нейтральное лицо(для таймера)
+                    Image emotion = Image.FromFile("neutral.png");
+                    
+                    Emotion.Width = 65;
+                    Emotion.Height = 65;
+                    Emotion.SizeMode = PictureBoxSizeMode.Zoom;
+
+                    if (smileDetected.Length > 0)
+                    {
+                        emotion = Image.FromFile("happy.png");
+                    Timers[1] = true;
+                    TimerNet = false;
+                    proc += koof * 1;
+                    }
+                    else
+                    {
+                        if (eyesDetected.Length == 0)
+                        {
+                            emotion = Image.FromFile("sleep.png");
+                        Timers[4] = true;
+                        TimerNet = false;
+                        netproc += koof * 0.5;
+                    }
+                        if (eyesDetected.Length == 1)
+                        {
+                            emotion = Image.FromFile("wink.png");
+                        Timers[3] = true;
+                        TimerNet = false;
+                        proc += koof * 0.5;
+                    }
+                        if ((mouthDetected.Length == 0 && mouth2Detected.Length == 0) && eyesDetected.Length > 1)
+                        {
+                            emotion = Image.FromFile("sad.png");
+                        Timers[2] = true;
+                        TimerNet = false;
+                        proc -= koof * 1;
+                    }
+                    }
+                if (TimerNet)
+                {
+                    Timers[0] = true;
+                    netproc += koof * 0.5;
+                }
+                Point P = new Point(f.X - 40, f.Y - 65);
+                Emotion.Location = P;
+                Emotion.Image = emotion;
+                Emotion.Parent = imageBox1;
+                Emotion.BackColor = Color.Transparent;
+            }
+            YpravTimer(Timers);
+            Shkala(proc, netproc);
+            imageBox1.Image = CameraImage;//отрисовывает изображение на элементе формы
+        }
+        private void Shkala(double P, double N)
+        {
+            if (Math.Abs(P) >= N)
+            {
+                if (P < 0)
+                    P += N;
+                if (P > 0)
+                    P -= N;
+            }
+            else P = 0;
+            Point K = new Point(717, 394);
+            K.Y -= (int)Math.Ceiling(132 * P);
+            Strelka.Location = K;
+        }
+        private void YpravTimer(bool[] T)
+        {
+            if (T[0])
+                NeturalT.Start();
+            else
+                NeturalT.Stop();
+            if (T[1])
+                HappyT.Start();
+            else
+                HappyT.Stop();
+            if (T[2])
+                SadT.Start();
+            else
+                SadT.Stop();
+            if (T[3])
+                WinkT.Start();
+            else
+                WinkT.Stop();
+            if (T[4])
+                SleepT.Start();
+            else
+                SleepT.Stop();
+        }
+        private void timerOb_Tick(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(label6.Text);
+            i++;
+            label6.Text = i.ToString();
+        }
+
+        private void NeturalT_Tick(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(label1.Text);
+            i++;
+            label1.Text = i.ToString();
+        }
+
+        private void HappyT_Tick(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(label2.Text);
+            i++;
+            label2.Text = i.ToString();
+        }
+
+        private void SadT_Tick(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(label3.Text);
+            i++;
+            label3.Text = i.ToString();
+        }
+
+        private void WinkT_Tick(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(label4.Text);
+            i++;
+            label4.Text = i.ToString();
+        }
+
+        private void SleepT_Tick(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(label5.Text);
+            i++;
+            label5.Text = i.ToString();
+        }
+    }
+
+}
+
